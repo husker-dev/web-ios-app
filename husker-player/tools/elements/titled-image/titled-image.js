49322fccc0ef4ff9aee0fdd961afaa9903d42a14
Nikita Shtengauer <redfancoestar@gmail.com>
2023-05-20 22:25:44 +0300

    Add files via upload

---
+++ husker-player/tools/elements/titled-image/titled-image.js
@@ -0,0 +1,16 @@
+﻿
+class TitledImage extends HTMLElement {
+
+	connectedCallback() {
+		this.innerHTML = `
+			<div id="l1">в поисках вдохновения</div>
+			<div id="l2">April - Пазлы по номерам</div>
+			<div id="l3">Собирайте необычные пазлы</div>
+			<div id="auto-ratio">
+				<img id="image" src="resources/image.jpg"/>
+			</div>
+		`;
+	}
+}
+
+window.customElements.define('titled-image', TitledImage);
