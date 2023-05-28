daaf109245e5dde1090c184f3f99fbfe39b4b23a
Nikita Shtengauer <redfancoestar@gmail.com>
2023-05-28 08:56:18 +0300

    Add files via upload

--- tools/elements/horizontal-scroll/horizontal-scroll.js
+++ tools/elements/horizontal-scroll/horizontal-scroll.js
@@ -2,7 +2,16 @@
 class HorizontallScroll extends HTMLElement {
 
 	connectedCallback() {
-		
+		console.log(this);
+		this.childNodes.forEach(node => {
+			if(node.outerHTML === undefined)
+				return;
+			const wrapper = document.createElement("div");
+			wrapper.classList.add("horizontal-scroll-element");
+			wrapper.innerHTML = node.outerHTML;
+
+			node.parentNode.replaceChild(wrapper, node);
+		});
 	}
 }
 
